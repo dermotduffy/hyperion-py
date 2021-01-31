3c1bdd80ba7255cbc2c2519d5dbd2e7b601a79ce
Dermot Duffy <dermot.duffy@gmail.com>
2021-01-30 22:50:56 -0800

    Bump version to v0.7.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="hyperion-py",
-    version="0.7.1",
+    version="0.7.2",
     author="Dermot Duffy",
     author_email="dermot.duffy@gmail.com",
     description="Hyperion Ambient Lighting Python Package",
